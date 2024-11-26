a286c642318053731facbb7816ccb1c464a8d13c
Gangadhar B <gangadharbavi312004@gmail.com>
2024-11-26 21:23:22 +0530

    Update and rename index.html to fibonacci.cpp

---
+++ fibonacci.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+// Function to calculate Fibonacci series
+void fibonacci(int n) {
+    int t1 = 0, t2 = 1, nextTerm;
+
+    cout << "Fibonacci Series: " << t1 << ", " << t2 << ", ";
+
+    for (int i = 1; i <= n - 2; ++i) {
+        nextTerm = t1 + t2;
+        t1 = t2;
+        t2 = nextTerm;
+        cout << nextTerm << ", ";
+    }
+}
+
+int main() {
+    int n;
+    cout << "

7b035ec1001cec2d95b24a9d78271eaca787b7a5
Gangadhar B <gangadharbavi312004@gmail.com>
2025-07-15 21:20:43 +0530

    Update reverse_pattern.cpp

--- pattern_problems/reverse_pattern.cpp
+++ pattern_problems/reverse_pattern.cpp
@@ -1,4 +1,14 @@
-#include<iostream.h>
+#include<iostream>
 using namespace std;
-int main{
-  
+int main(){
+    int n;
+    cout << "Enter the number of lines:";
+    cin >> n;
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j>0;j--){
+            cout << j;
+        }
+        cout << endl;
+    }
+    return 0;
+}

041a969211cd0016fb49f1927c79cdbbb7c564c3
gangadharbhavi-7 <gangadharbavi312004@gmail.com>
2026-03-21 13:46:51 +0530

    Add program to check whether a year is leap year or not

--- cpp_second_pu/disc_amount.cpp
+++ cpp_second_pu/disc_amount.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-    double amount,disc;
+    double amount,disc=0.00;
     cout<<"Enter the bill amount: ";
     cin>>amount;
     if(amount>1000){

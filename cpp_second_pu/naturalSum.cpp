08c1090485c0b88e14e0c2f20da145471380b093
gangadharbhavi-7 <gangadharbavi312004@gmail.com>
2026-04-06 21:54:52 +0530

    Add cpp program to compute the sum of n natural numbers inputed by the user

---
+++ cpp_second_pu/naturalSum.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,sum=0;
+    cout<<"Enter the number of elements: ";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        sum+=i;
+    }
+    cout<<"The sum of first "<<n<<" natural numbers is: "<<sum;
+    return 0;
+}

29bdd69a17199dd9cae084a8bff49f0715ca2795
gangadharbhavi-7 <gangadharbavi312004@gmail.com>
2026-04-07 13:13:20 +0530

    Add a cpp program to demonstrate function overriding from classes

---
+++ cpp_second_pu/ClassDemo.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+using namespace std;
+class Shape{
+    public : 
+    virtual void display(){
+        cout<<"This is a shape"<<endl;
+
+    }
+};
+ 
+class circle: public Shape{
+    public: 
+    void display() override{
+        cout<<"This is a circle"<<endl;
+    }
+};
+
+class rectangle: public Shape{
+    public:
+    void display() override{
+        cout<<"This is a rectangle"<<endl;
+    }
+};
+
+class triangle : public Shape{
+    public: 
+    void display() override{
+        cout<<"This is a triangle"<<endl;
+    }
+};
+ 
+class polygon: public Shape{
+    public :
+    void display() override{
+        cout<<"This is a polygon"<<endl;
+    }
+};
+
+class square: public rectangle{
+    public:
+    void display() override{
+        cout<<"This is a square"<<endl;
+    }
+};
+
+int main(){
+    Shape s;
+    circle c;
+    rectangle r;
+    triangle t;
+    polygon p;
+    square sq;
+    s.display();
+    c.display();
+    r.display();
+    t.display();
+    p.display();
+    sq.display();
+    return 0;
+}

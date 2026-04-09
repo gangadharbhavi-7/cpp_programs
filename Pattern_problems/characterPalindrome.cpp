f8fa860d6d41de691f6e95ae6a779e9a97baa59b
gangadharbhavi-7 <gangadharbavi312004@gmail.com>
2026-04-09 19:02:40 +0530

    Add cpp program to print palindromic character sequence'

---
+++ Pattern_problems/characterPalindrome.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the number of rows: ";
+    cin>>n;
+    
+    for(int i=0;i<n;i++){
+        char ch='A';
+        for(int j=0;j<i+1;j++){
+            cout<<ch;
+            ch++;
+        }
+        ch=ch-2;
+            for(int j=0;j<i;j++){
+               
+                cout<<ch;
+                ch--;
+                    
+            }
+        
+        cout<<endl;
+    }
+    return 0;
+}

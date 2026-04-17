b9fb865e9226fe8d18c6e4afbccb543161f116b1
Gangadhar B <gangadharbavi312004@gmail.com>
2026-04-17 23:26:35 +0530

    Fix syntax error in naturalSum.cpp

--- cpp_second_pu/naturalSum.cpp
+++ cpp_second_pu/naturalSum.cpp
@@ -4,7 +4,7 @@ int main(){
     int n,sum=0;
     cout<<"Enter the number of elements: ";
     cin>>n;
-    for(int i=1;i<=n;i++){gang
+    for(int i=1;i<=n;i++){
         sum+=i;
     }
     cout<<"The sum of first "<<n<<" natural numbers is: "<<sum;

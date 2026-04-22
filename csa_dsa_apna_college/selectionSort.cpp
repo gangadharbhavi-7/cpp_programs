9a3a8615e94b86567506d8b328e11e9befe809f0
gangadharbhavi-7 <gangadharbavi312004@gmail.com>
2026-04-22 18:23:58 +0530

    Add a program to demonstrate the selection sort

---
+++ csa_dsa_apna_college/selectionSort.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[5]={2,1,5,3,7};
+    int n = sizeof(arr)/sizeof(int);
+    for(int i =0;i<n;i++){
+        int minu=arr[i];
+        for(int j=0;j<n;j++){
+            if(arr[j]<=arr[i]){
+                minu=arr[j];
+                swap(minu,arr[j]);
+            }
+        }
+        
+    }
+    for(int i=0;i<n;i++){
+        cout<<arr[i];
+    }
+    return 0;
+
+}
